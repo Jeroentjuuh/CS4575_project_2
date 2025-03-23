cebc6e5c1d87a7ddca4604d0ef81ebf50a92591f
RoelofvdG <r.r.l.vandergeest@student.tudelft.nl>
2025-03-23 16:28:12 +0100

    Add inserting of surefire config for projects that do not have it, move joularjx csvs to separate folder

--- scripts/run_energy_tests.py
+++ scripts/run_energy_tests.py
@@ -32,6 +32,7 @@ repos = [
 	"https://github.com/forcedotcom/dataloader.git",
 	"https://github.com/forge/roaster.git"
 ]
+# repos = repos[:2]
 
 if __name__ == "__main__":
 	# Create directories if they don't exist
@@ -39,12 +40,14 @@ if __name__ == "__main__":
 	Path("./logs").mkdir(exist_ok=True)
 	Path("./logs").mkdir(exist_ok=True)
 	Path("./joularjx").mkdir(exist_ok=True)
+	Path("./results").mkdir(exist_ok=-True)
 
 	# Build joularjx
 	joularjx_dir = Path(os.getcwd(), "joularjx")
 	run(["git", "clone", "https://github.com/joular/joularjx.git", joularjx_dir])
+	shutil.copy(Path("config.properties"), Path(joularjx_dir, "config.properties"))
 	os.chdir(joularjx_dir)
-	r = run("mvn clean install -DskipTests", shell=True)
+	# r = run("mvn clean install -DskipTests", shell=True)
 	joularjx_path = Path(joularjx_dir, "target", list(filter(lambda x: x.endswith(".jar") and "joularjx" in x, os.listdir(Path(joularjx_dir, "target"))))[0])
 	print(f"JoularJX path: {joularjx_path}")
 	os.chdir(joularjx_dir.parents[0])
@@ -57,15 +60,12 @@ if __name__ == "__main__":
 		run(["git", "clone", repo, project_dir])
 
 		print(f"Building and testing {project}...")
-		
-		os.chdir(project_dir)
 
-		shutil.rmtree(f"{project_dir}/joularjx-result", ignore_errors=True)
-		shutil.copy('../../config.properties', f"{project_dir}/config.properties")
-		# config = configparser.ConfigParser()
-		# config.read(f"{project_dir}/config.properties")
+		shutil.copy(Path("config.properties"), Path(project_dir, "config.properties"))
+
+		os.chdir(project_dir)
+		
 		log_path = Path("../../logs", project + "_build.log")
-		succeeded = False
 
 		if Path("pom.xml").exists():
 			print("Maven project")
@@ -78,10 +78,11 @@ if __name__ == "__main__":
 			tree = ET.parse(Path(project_dir, "pom.xml"))
 			groupId = tree.find(f"{prefix}groupId").text
 
-			# config.set("filter-method-names", "filter-method-names", groupId)
+			found_surefire = 0
 			for plugin in tree.iter(f"{prefix}plugin"):
 				artifactId = plugin.find(f"{prefix}artifactId").text
 				if "maven-surefire-plugin" in artifactId:
+					found_surefire += 1
 					if plugin.find(f"{prefix}configuration") is None:
 						plugin.append(ET.Element(f"{prefix}configuration"))
 					configuration = plugin.find(f"{prefix}configuration")
@@ -92,12 +93,32 @@ if __name__ == "__main__":
 						argLine.text = f"-javaagent:\"{joularjx_path}\""
 					elif "joularjx" not in argLine.text.lower():
 						argLine.text = f"-javaagent:\"{joularjx_path}\" {argLine.text}"
+			if found_surefire > 0:
+				print(f"Found surefire plugin in {project}")
+			else:
+				print(f"No surefire in {project}")
+				surefire_element = ET.fromstring("""<plugin>
+          <artifactId>maven-surefire-plugin</artifactId>
+          <version>3.5.2</version>
+          <configuration>
+
+            <trimStackTrace>false</trimStackTrace>
+            <argLine>-javaagent:"/home/roelof/Repositories/CS4575_project_2/joularjx/target/joularjx-3.0.1.jar"</argLine>
+          </configuration>
+        </plugin>""")
+				for plugins_list in tree.iter(f"{prefix}plugins"):
+					plugins_list.append(surefire_element)
 			tree.write("pom.xml")
 
 			with open(log_path, "w") as outfile:
 				r = run("mvn clean test", shell=True, stdout=outfile, stderr=outfile)
+			for csv_file in Path(project_dir, "joularjx-result").rglob("*.csv"):
+				print(csv_file.name)
+				csv_name = f"{project}-" + "-".join(csv_file.name.split("-")[2:])
+				shutil.move(csv_file.resolve(), Path("../", "../", "results", csv_name))
 		elif Path("build.gradle").exists() or Path("build.gradle.kts").exists():
-			print("Gradle project")
+			print("Gradle project, skipping...")
+			continue
 			with open(log_path, "w") as outfile:
 				run("gradle clean test", shell=True, stdout=outfile, stderr=outfile)
 		else:
